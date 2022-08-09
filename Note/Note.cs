046774ab0f43e9ef0ffc14fa7b47145df84023f3
Artem <gdtechai@gmail.com>
2022-08-09 18:04:09 +0700

    feature(Note): added note class

---
+++ Note/Note.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Note;
+
+internal class Note
+{
+	private const int MaxTitleLength = 50;
+
+	private string _title = "Без названия";
+
+	public string Title
+	{
+		get => _title;
+		set
+		{
+			if (string.IsNullOrWhiteSpace(value))
+				throw new ArgumentNullException("value");
+
+			if (value.Length > MaxTitleLength)
+				throw new ArgumentException("value");
+
+			_title = value;
+		}
+	}
+
+	private NoteCategory Category { get; set; } = NoteCategory.Work;
+
+	private DateTime CreationTime { get; init; }
+
+	private DateTime LastModifyTime { get; set; }
+
+	private StringBuilder NoteText { get; set; }
+}
